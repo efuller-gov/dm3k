67172cbd3c84ff991a78973eff148cc5305a5e32
cancrgj1 <George.Cancro@jhuapl.edu>
2021-08-02 14:42:05 -0400

    Had to remove Flask_restful because of dependency on aniso library that was GPL

--- api/views.py
+++ api/views.py
@@ -15,9 +15,8 @@ API Endpoints for open-dm3k API
 import os
 import sys
 
-from flask import request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response
 from flask import current_app as app
-from flask_restful import Resource
 
 # ensure that optimizer directory is in path
 app_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
@@ -26,77 +25,67 @@ if app_directory not in sys.path:
 
 from optimizer.slim_optimizer_main import create_opt
 
-class Version(Resource):
+api = Blueprint('api', __name__)
+
+@api.route('/api/version', methods=['GET'])
+def get_version():
     """
-    Simple Endpoint for checking aliveness and providing API version
+    GET /api/version
+
+    :return dict version: the version of the current API
     """
-    def __init__(self):
-        pass
+    return {'api_version': "1.0"}
 
-    def get(self):
-        """
-        GET /api/version
-
-        :return dict version: the version of the current API
-        """
-        return {'api_version': "1.0"}
-
-class VizInput(Resource):
+@api.route('/api/vizdata', methods=['POST'])
+def post_vizdata():
     """
-    Endpoint for taking in data from the UI
+    POST /api/vizdata
+
+    :return dict response: results of optimization of post data
+        (for full POST API details see /docs/api_devGuide.md)
     """
-    def __init__(self):
-        pass
 
-    def post(self):
-        """
-        POST /api/vizdata
+    app.logger.info("Viz Data POST")
+    app.logger.debug(request.get_data())
 
-        :return dict response: results of optimization of post data
-            (for full POST API details see /docs/api_devGuide.md)
-        """
-    
-        app.logger.info("Viz Data POST")
-        app.logger.debug(request.get_data())
+    input_dict = request.json
+    app.logger.debug(input_dict)
 
-        input_dict = request.json
-        app.logger.debug(input_dict)
+    config = {"optimizer": input_dict['algorithm']}
 
-        config = {"optimizer": input_dict['algorithm']}
+    opt, validation_errors = create_opt(input_dict, config)
 
-        opt, validation_errors = create_opt(input_dict, config)
-
-        if len(validation_errors) > 0:
-            app.logger.warning("VALIDATION ERRORS...")
-            for e in validation_errors:
-                app.logger.warning(e)
-            
-            # TODO - we should update this
-            err_response = {
-                "body": validation_errors,
-                "reason": "Validation Errors in input data",
-                "statusCode": 400
-            }
-
-            return jsonify(err_response)
-        else:
-            app.logger.debug("No Validation errors")
-
-        opt.build()
-        opt.solve()
-        results = opt.get_output()
-
-        all_results = results.to_dict()
-        all_results["allocations"] = results.get_allocations()
-        all_results["objective_value"] = results.get_objective_value()
-
-        # TODO - we should update this 
-        response = {
-            "body": all_results,
-            "reason": "OK",
-            "statusCode": 200
+    if len(validation_errors) > 0:
+        app.logger.warning("VALIDATION ERRORS...")
+        for e in validation_errors:
+            app.logger.warning(e)
+        
+        # TODO - we should update this
+        err_response = {
+            "body": validation_errors,
+            "reason": "Validation Errors in input data",
+            "statusCode": 400
         }
 
-        return jsonify(response)
+        return jsonify(err_response)
+    else:
+        app.logger.debug("No Validation errors")
+
+    opt.build()
+    opt.solve()
+    results = opt.get_output()
+
+    all_results = results.to_dict()
+    all_results["allocations"] = results.get_allocations()
+    all_results["objective_value"] = results.get_objective_value()
+
+    # TODO - we should update this 
+    response = {
+        "body": all_results,
+        "reason": "OK",
+        "statusCode": 200
+    }
+
+    return jsonify(response)
 
 
